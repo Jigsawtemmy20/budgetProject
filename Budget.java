7aa11a1bb0e08460cc47bf790d32cdda3645fc30
Jigsawtemmy20 <artyczhang24@gmail.com>
2025-04-23 16:28:49 -0400

    this is what I wrote so far

---
+++ Budget.java
@@ -0,0 +1,13 @@
+public class Budget {
+	private double total;
+	private ArrayList<String> categories;
+	public budget(double total) {
+		this.total = total;
+	}
+	public void setTotal(double total) {
+		this.total = total;
+	}
+	public double getTotal() {
+		return this.total;
+	}
+}
---
+++ BudgetProject.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class BudgetProject {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		boolean on = true;
+		while(on) {
+			System.out.println("Welcome to the Budget Calculator!  What would you like to do?\n1) Make a new Budget\n2) Edit Budget\n3) View Budget\n4) exit");
+			int response = input.nextInt();
+			switch(response) {
+			case 1:
+				System.out.print("amount: ");
+				double total = input.nextDouble();
+				System.out.print("number of spending categories: ");
+				int categories = input.nextInt();
+				budget yourBudget = new breakudget(total);
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				on = false;
+				System.out.println("goodbye!");
+				input.close();
+				break;
+			default:
+				System.out.println("try again");
+			}
+		}
+	}
+}
