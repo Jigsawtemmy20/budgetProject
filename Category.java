72ca9be901454822dc371faab2197593cb67cf23
Jigsawtemmy20 <artyczhang24@gmail.com>
2025-04-28 11:10:25 -0400

    made a category class

---
+++ Category.java
@@ -0,0 +1,26 @@
+public class Category {
+    private String name;
+    private double portion;
+    public Category(double portion, String name){
+        this.portion = portion;
+        this.name = name;
+    }
+    public double getPortion(){
+        return this.portion;
+    }
+    public void setPortion(double portion){
+        this.portion = portion;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public double getAmount(double total){
+        return total*this.getPortion();
+    }
+    public String toString(){
+        return("name: " + this.getName() + "\nportion: " + this.getPortion() + "%");
+    }
+}
