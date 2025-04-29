5afb568fe037c8522f1675d5632f4bf4809d8317
Jigsawtemmy20 <artyczhang24@gmail.com>
2025-04-29 01:02:32 -0400

    worked on this a bit more

--- Budget.java
+++ Budget.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 public class Budget {
 	private double total;
-	private ArrayList<String> categories;
+	private ArrayList<Category> categories;
 	public Budget(double total) {
 		this.total = total;
 	}
@@ -10,5 +10,11 @@ public class Budget {
 	}
 	public double getTotal() {
 		return this.total;
+	}
+	public void addCategory(String name, double portion){
+		categories.add(new Category(name, portion));
+	}
+	public void removeCategory(Category c){
+		categories.remove(c);
 	}
 }
